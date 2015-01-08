e24def80c48004786166d0344c7009db082d5f37
Matt Frazier <maf7sm@virginia.edu>
2015-01-08 16:11:42 -0500

    Cleanup fix .gitignore

--- dor/admin.py
+++ dor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from treebeard.admin import TreeAdmin
+from treebeard.forms import movenodeform_factory
+from dor.models import Taxonomy
 
-# Register your models here.
+class MyAdmin(TreeAdmin):
+    form = movenodeform_factory(Taxonomy)
+
+admin.site.register(Taxonomy, MyAdmin)

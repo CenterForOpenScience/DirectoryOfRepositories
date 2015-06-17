48478091bf82dbc8f67f671d4cfae19809a47381
icereval <icereval@gmail.com>
2015-06-17 10:23:36 -0400

    allow for local.py settings override

--- RepoDir/settings/defaults.py
+++ RepoDir/settings/defaults.py
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -124,4 +124,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(os.path.dirname(__file__),'static'),)
+STATICFILES_DIRS = (os.path.realpath(os.path.join(os.path.dirname(os.path.dirname(__file__)),'static')),)
